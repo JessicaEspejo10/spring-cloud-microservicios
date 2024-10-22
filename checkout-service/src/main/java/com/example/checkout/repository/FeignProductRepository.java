87bc7daa8ac9b56130b6ab656d04981205123e4a
Jessica Espejo <nataespejot@gmail.com>
2024-10-22 08:01:04 -0500

    Inclusiòn de ruteo con spring cloud gateway

--- checkout-service/src/main/java/com/example/checkout/repository/FeignProductRepository.java
+++ checkout-service/src/main/java/com/example/checkout/repository/FeignProductRepository.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name="external-products",url="http://localhost:8089")
+@FeignClient(name="products-service")
 public interface FeignProductRepository {
     @RequestMapping(method = RequestMethod.GET, value="/products")
     Product getProductById(@RequestParam String id);
